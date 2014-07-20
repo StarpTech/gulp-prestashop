7e172edc196d835bd09497c90316ff4c0c558d8c
Dustin Deus <deusdustin@gmail.com>
2014-07-20 14:58:41 +0200

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
-gulp-prestashop
-===============
+# gulp-prestashop-boilerplate
 
-Gulp prestashop build system
+## Getting Started
+
+Install dependencies:
+
++ Install Ruby (For windows http://rubyinstaller.org/)
++ ```gem install sass```
++ ```gem install compass```
++ ```npm install```
+
+## Usage
+
+### 1. Specifiy your project paths in the gulpfile.js
+```var psProjectDir; // Your root prestashop dir (e.g prestashop)```
+```var themeName; // Your active theme (e.g default-bootstrap)```
+
+### Tasks
+
+#### Run the default task and watch all files
+```bash
+$ gulp
+```
+
+#### Run just linting
+```bash
+$ gulp lint
+```
+
+#### Run just sass
+```bash
+$ gulp sass
+```v
---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+'use strict';
+
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var sass = require('gulp-ruby-sass');
+var path = require('path');
+var minifycss = require('gulp-minify-css');
+
+//General
+var psProjectDir = 'prestashop';
+var themeName = 'default-bootstrap';
+var projectDir = path.resolve(__dirname);
+
+var paths = {
+    prestashopSassFiles: './' + psProjectDir + '/themes/default-bootstrap/sass/**/*.scss',
+    prestashopCssDir: './' + psProjectDir + '/themes/default-bootstrap/css',
+    prestashopJsFiles: './' + psProjectDir + '/themes/default-bootstrap/js/**/*.js',
+    prestashopJsDir: './' + psProjectDir + '/themes/default-bootstrap/js/'
+};
+var sassConfig = {
+    style: 'expanded',
+    compass: true,
+    loadPath: [projectDir + '/prestashop/themes/'+ themeName +'/sass']
+};
+
+/*
+* Custom routine to fail gulp when jshint is failed
+* (Currently not implemented in gulp-jshint :/)
+*/
+var map = require('map-stream');
+var exitOnJshintError = map(function (file, cb) {
+    if (!file.jshint.success) {
+        console.error('jshint failed');
+        process.exit(1);
+    }
+});
+
+/* Task
+* Lint all prestashop theme javascript files
+*/
+gulp.task('lint', function() {
+    return gulp.src(paths.prestashopJsFiles)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(exitOnJshintError);
+});
+
+/* Task
+* Compile our prestashop SASS files
+*/
+gulp.task('sass', function() {
+    return gulp.src(paths.prestashopSassFiles)
+        .pipe(sass(sassConfig))
+        .pipe(gulp.dest(paths.prestashopCssDir));
+});
+
+/* Task
+* Watch Files For Changes
+*/
+gulp.task('watch', function() {
+    gulp.watch(paths.prestashopJsFiles, ['lint']);
+    gulp.watch(paths.prestashopSassFiles, ['sass']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'sass', 'watch']);
